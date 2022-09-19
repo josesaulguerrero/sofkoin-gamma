d529e11c9b317a45d26f7e25789b9ae232d700f2
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-19 13:41:57 -0500

    Set up project skeleton with Spring Initializer

---
+++ src/test/java/co/com/sofkoin/gamma/GammaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkoin.gamma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
